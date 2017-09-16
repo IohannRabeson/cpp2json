0c67e13e848dddca6e9e1bc7c733256fd213ac5b
irabeson <IohannRabeson@users.noreply.github.com>
2017-09-16 19:34:52 +0200

    Added travis config.

---
+++ scripts/travis/run_tests.sh
@@ -0,0 +1,13 @@
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo "usage: unix_build.sh <build_dir_name>"
+	echo "<build_dir_name>: Name of the directory created and used to build the project."
+	exit 1
+fi
+
+BUILD_DIR_NAME="$1"
+
+cd $BUILD_DIR_NAME
+ctest --output-on-failure
+cd -
