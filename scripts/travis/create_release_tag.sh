bfceb2ec7333a023e32b12caa27d2faaaf5f60bc
irabeson <IohannRabeson@users.noreply.github.com>
2017-12-08 21:38:34 -0500

    Add shebangs to shell scripts.

--- scripts/travis/create_release_tag.sh
+++ scripts/travis/create_release_tag.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git tag $VERSION_NUMBER -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER."
