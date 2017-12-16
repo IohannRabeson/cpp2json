c28e1713a24f9def6d41851f06c2b898eff5aad0
irabeson <IohannRabeson@users.noreply.github.com>
2017-12-16 02:01:01 -0500

    Fix test build.

--- sources/tests/gtest.cpp
+++ sources/tests/gtest.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 
+// TODO: Cpp2JsonVisitorFixture en utilisant le main de l'application.
+// En particulier on a juste besoin des extra arguments:
+//
+//      clang::tooling::CommandLineArguments extraArguments = makeLlvmIncludeArguments();
+//      auto extra = clang::tooling::getInsertArgumentAdjuster(extraArguments, clang::tooling::ArgumentInsertPosition::END);
+//
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
