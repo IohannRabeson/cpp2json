f72951d0b8a25176333567ab01e161768460a251
irabeson <IohannRabeson@users.noreply.github.com>
2017-12-20 21:01:29 -0500

    Update README.

---
+++ sources/tests/resources/test_example.hpp
@@ -0,0 +1,13 @@
+#ifndef CPP2JSON_TEST_EXAMPLE_HPP
+#define CPP2JSON_TEST_EXAMPLE_HPP
+#include <string>
+
+struct Example01
+{
+    float m_float;
+    std::string m_string;
+    int m_int;
+    double m_double;
+};
+
+#endif //CPP2JSON_TEST_EXAMPLE_HPP
