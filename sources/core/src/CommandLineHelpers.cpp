d0ade2547b8de9c3db8d54f57432de66b8401abd
irabeson <iohannrabeson@users.noreply.github.com>
2021-07-28 22:01:37 -0400

    Ci

--- sources/core/src/CommandLineHelpers.cpp
+++ sources/core/src/CommandLineHelpers.cpp
@@ -8,6 +8,7 @@ clang::tooling::CommandLineArguments makeLlvmIncludeArguments()
 
     std::string const llvmVersion = LlvmBasePath.substr(LlvmBasePath.find_last_of('/'));
     clang::tooling::CommandLineArguments extraArguments{
+        "-std=c++14",
         "-I" + LlvmBasePath + "/include/c++/v1",
         "-I" + LlvmBasePath + "/lib/clang/" + llvmVersion + "/include",
     };
